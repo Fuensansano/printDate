dc9cc817f0aa6e99d8234dd88bb5b46877af7e7b
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-06-15 17:49:36 +0200

    red should_print_a_date

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -2,6 +2,17 @@ import java.time.LocalDate;
 
 public class Calendar {
     public static LocalDate today() {
-        return LocalDate.now();
+        return createRandomDate();
+    }
+
+    private static int createRandomIntBetween(int start, int end) {
+        return start + (int) Math.round(Math.random() * (end - start));
+    }
+
+    private static LocalDate createRandomDate() {
+        int day = createRandomIntBetween(1, 28);
+        int month = createRandomIntBetween(1, 12);
+        int year = createRandomIntBetween(1900, 2000);
+        return LocalDate.of(year, month, day);
     }
 }
